f7eb1d6a985871a5b62fd0ec0f87a689d220d606
Vinicius Schutt <viniciuschutt@gmail.com>
2021-05-17 15:59:51 -0300

    Corrections and code descriptions

--- Algoritmos/-999.py
+++ Algoritmos/-999.py
@@ -1,5 +1,7 @@
-n = float(input("Informe um número: "))
-while n != -999:
-    triplo=n*3
-    print("Triplo do número: ", triplo)
-    n=float(input("Informe um número: "))
+# simple code to give the triple ou any number informed
+
+n = float(input("Inform a number: "))
+while n != -10000: # -10000 is a random example
+    triple = n*3
+    print("The triple of the number is: ", triple)
+    n = float(input("Inform a number: "))
