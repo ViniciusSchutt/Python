0881885d270057c078d83a83e6263a6c646645bb
Vinicius Schutt <viniciuschutt@gmail.com>
2021-05-26 20:53:58 -0300

    Adding description

--- Hacker Rank/123n.py
+++ Hacker Rank/123n.py
@@ -1,7 +1,8 @@
 #Without using any string methods, try to print the following:
 # 123... n
 
-n=int(input())
+#Get number
+n = int(input("Do you want to print until what number? "))
 
 for a in range(n):
     print(a+1, end="")
