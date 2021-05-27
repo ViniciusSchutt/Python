9c22a37d804212afed6d39a18e0d978d6f3ea98a
Vinicius Schutt <viniciuschutt@gmail.com>
2021-05-27 18:56:02 -0300

    Adding descriptions.

--- Hacker Rank/first name last name print.py
+++ Hacker Rank/first name last name print.py
@@ -1,7 +1,9 @@
+#Defining the function to print full name that receives 2 arguments as inputs (first_name and last_name)
 def print_full_name(a, b):
     print('Hello', a, b +'! You just delved into python.')
 
 if __name__ == '__main__':
-    first_name = input()
-    last_name = input()
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    #Calling the function
     print_full_name(first_name, last_name)
