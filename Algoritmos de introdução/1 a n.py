e3a4d123e863dc717d9f8fc342a130bcc06ba959
Vinicius Schutt <viniciuschutt@gmail.com>
2021-06-03 22:18:21 -0300

    Mudanças pontuais para receber input do usuário.

---
+++ Algoritmos de introdução/1 a n.py
@@ -0,0 +1,7 @@
+# Código simples que imprime todos os números de 1 a n
+i = 1
+n = int(input("Digite o valor de n: ")) # o contador inicia em 1
+while i < n + 1: # enquanto i for menor que n + 1
+    print("", i) # imprima-o
+    i = i + 1 # e o incremente
+
---
+++ Exercicios simples/Lista 1/analise de conjunto de numeros.py
@@ -0,0 +1,29 @@
+# Escreva um programa que leia um conjunto de números inteiros. Calcule e mostre:
+# a)o menor número
+# b)a soma dos números pares e maiores que 10
+# c)a quantidade de números ímpares
+# d)a média dos números maiores que 20
+
+soma = 0
+media = 0
+contador = 0
+impares = 0
+lista = []
+quantidade = int(input("Quantos números deseja analisar?:"))
+
+for i in range(quantidade):
+    num = int(input("Digite um número: "))
+    lista.append(num)
+    if num > 10 and num % 2 == 0:
+        soma = soma + num
+    if num % 2 != 0:
+        impares = impares + 1
+    if num > 20:
+        contador = contador + 1
+    media = media + num
+
+print(lista)
+print("O menor número da sua lista: ", min(lista))
+print("A soma dos números pares e maiores que 10: ", soma)
+print("A quantidade de números ímpares: ", impares)
+print("A media dos números maiores que 20 eh: ", media/contador)
