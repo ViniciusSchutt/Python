dc361e6934ca350215e08304fe91423055d79d81
Vinicius Schutt <viniciuschutt@gmail.com>
2021-06-08 19:11:26 -0300

    refactor

---
+++ Exercicios simples/Lista 1/Media idades.py
@@ -0,0 +1,12 @@
+# Média idades
+
+idades = []
+x = 1
+xx = 0
+while x:
+    x = int(input('Digite uma idade ou 0 para mostrar a média de idades: '))
+    if x:
+        idades.append(x)
+        xx = xx+1
+        
+print('A média de idade é: ', sum(idades)/xx)
